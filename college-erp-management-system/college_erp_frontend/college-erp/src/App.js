7b1bec5b1c55cd9e22887d41bacf3e08b0664e60
zuber144 <yaseenmohammedyaseen264@gmail.com>
2025-02-26 20:36:31 +0530

    fetched student api

--- college-erp-management-system/college_erp_frontend/college-erp/src/App.js
+++ college-erp-management-system/college_erp_frontend/college-erp/src/App.js
@@ -5,20 +5,20 @@ import axios from "axios";
 const App = () => {
   const [formData, setFormData] = useState("");
   const handleChange = (e) => {
-    setFormData(e.target.value)
-  }
+    setFormData(e.target.value);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        `http://localhost:8080/student/login?registrationNumber=${formData}`, // ✅ RequestParam format
+        `/student/login?registrationNumber=${formData}`, // ✅ RequestParam format
         null, // No request body
         {
           headers: { "Content-Type": "application/x-www-form-urlencoded" }, // ✅ Updated headers
         }
       );
-      alert(response.data.message)
+      alert(response.data.message);
       console.log("Login Successful: " + JSON.stringify(response.data));
     } catch (error) {
       console.error("Login failed:", error.response?.data || error.message);
@@ -28,16 +28,21 @@ const App = () => {
 
   return (
     <div className="container">
-      <form className="form-box" action="" onSubmit={handleSubmit} >
+      <form className="form-box" action="" onSubmit={handleSubmit}>
         <h2>Register</h2>
         <div className="input-group">
-          <label>Reg.no</label><input type="text" placeholder="Enter Reg. No" onChange={handleChange} required />
+          <label>Reg.no</label>
+          <input
+            type="text"
+            placeholder="Enter Reg. No"
+            onChange={handleChange}
+            required
+          />
         </div>
         <button className="submit-btn">Submit</button>
       </form>
     </div>
   );
-
 };
 // done something
 export default App;
